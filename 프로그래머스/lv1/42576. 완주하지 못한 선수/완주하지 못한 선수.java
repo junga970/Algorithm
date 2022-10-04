c47e05a665fc78a503684ebb19e464c94f4bf188
junga970 <111635735+junga970@users.noreply.github.com>
2022-10-04 10:24:30 +0900

    Update 완주하지 못한 선수.java

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -2,19 +2,22 @@ import java.util.HashMap;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
+        
         HashMap<String, Integer> hm = new HashMap<>();
-
+        
+        // key: 선수이름, value: 1로 추가, 동명이인이면 value + 1
         for (String s : participant) {
             if (hm.containsKey(s)) {
                 hm.put(s, hm.get(s) + 1);
             } else { hm.put(s, 1); }
         }
 
-
+        // 해당하는 선수 value에 -1
         for (String c : completion) {
             hm.put(c, hm.get(c) - 1);
         }
-
+        
+        // value가 1이상이면 완주하지 못한 선수이므로 key리턴
         for (String key : hm.keySet()) {
             if (hm.get(key) > 0) {
                 return key;
