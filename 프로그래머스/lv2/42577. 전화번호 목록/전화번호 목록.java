ea89ddfc8fb141f66a4597fa18f2b6435ed2e605
junga970 <111635735+junga970@users.noreply.github.com>
2022-10-05 21:54:21 +0900

    [level 2] Title: 전화번호 목록, Time: 230.17 ms, Memory: 96.9 MB -BaekjoonHub

--- 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.java
@@ -3,8 +3,10 @@ import java.util.Arrays;
 
 class Solution {
      public boolean solution(String[] phone_book) {
+         // [119, 1195524421, 97674223]
          Arrays.sort(phone_book);
-
+         
+         // startsWith(): 대상문자열이 비교문자열로 시작하면 true 반환, 아니면 false
          for (int i = 0; i < phone_book.length - 1; i++) {
              if(phone_book[i + 1].startsWith(phone_book[i])) {
                  return false;
@@ -14,22 +16,6 @@ class Solution {
          return true;
      }
     
-    
-    
-//      <효율성에서 실패한 코드2>    
-//     public boolean solution(String[] phone_book) {
-//         Arrays.sort(phone_book, (s1, s2) -> s1.length() - s2.length());
-        
-//         for (int i = 0; i < phone_book.length - 1; i++) {
-//             for (int j = i + 1; j < phone_book.length; j++) {
-//                 if(phone_book[j].startsWith(phone_book[i])) {
-//                     return false;
-//                 }
-//             }
-//         }
-//         return true;
-//     }
-        
     
     
 //      <효율성에서 실패한 코드>
