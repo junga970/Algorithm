532a1585989c96e6c36b8220f62079e147575aad
junga970 <111635735+junga970@users.noreply.github.com>
2022-10-10 00:22:55 +0900

    [level 2] Title: 올바른 괄호, Time: 16.15 ms, Memory: 54.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,22 @@
+import java.util.Stack;
+
+class Solution {
+    boolean solution(String s) {
+        Stack<Character> st = new Stack<>();
+
+        if (s.charAt(0) == ')') {
+            return false;
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                st.push('(');
+            } else if (st.isEmpty()) {
+                return false;
+            } else {
+                st.pop();
+            }
+        }
+        return st.isEmpty() ? true : false;
+    }
+}
