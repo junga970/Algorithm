81b8089ce5cb5048ba02327273266d86bff7084f
junga970 <111635735+junga970@users.noreply.github.com>
2022-10-19 20:03:50 +0900

    Update 입국심사.java

--- 프로그래머스/lv3/43238. 입국심사/입국심사.java
+++ 프로그래머스/lv3/43238. 입국심사/입국심사.java
@@ -1,29 +1,28 @@
-import java.util.*;
+import java.util.Arrays;
+
 class Solution {
-    public static long solution(int n, int[] times) {
-        long answer = Long.MAX_VALUE;
-        
+    public long solution(int n, int[] times) {
         Arrays.sort(times);
-        
-        long start = times[0];  
-        long end = (long)times[times.length-1]*(long)n;
-        
-        
-        long mid ;
-        long sum = 0;
-        while(start<=end) {
-            mid=  (start+end)/2;
-            sum = 0;
+
+        long start = times[0];
+        long end = (long)times[times.length - 1] * (long)n;
+        long mid = 0;
+        long answer = Long.MAX_VALUE;
+
+        while (start <= end) {
+            mid = (start + end) / 2;
+
+            long sum = 0;
             for(int time : times) {
-                sum+= mid/time;
+                sum += mid / time;
             }
-            
-            if(sum>=n) {
+
+            if (sum >= n) {
                 answer = Math.min(answer, mid);
-                end = mid-1;
+                end = mid - 1;
             }
             else {
-                start = mid+1;
+                start = mid + 1;
             }
         }
         return answer;
