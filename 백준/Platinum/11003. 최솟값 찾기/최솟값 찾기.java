943ccd22013fe08a4e4c3b0a52730b87501f153b
junga970 <111635735+junga970@users.noreply.github.com>
2025-05-12 19:02:17 +0900

    [Platinum V] Title: 최솟값 찾기, Time: 2312 ms, Memory: 618636 KB -BaekjoonHub

--- 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.java
+++ 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.java
@@ -1,48 +1,50 @@
+import java.util.*;
 import java.io.*;
-import java.util.Deque;
-import java.util.LinkedList;
-import java.util.StringTokenizer;
 
 public class Main {
-    static class Node {
-        int idx;
-        int value;
+  static int N, L;
 
-        public Node(int idx, int value) {
-            this.idx = idx;
-            this.value = value;
-        }
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+
+    N = Integer.parseInt(st.nextToken());
+    L = Integer.parseInt(st.nextToken());
+
+    // deque
+    Deque<Node> deque = new LinkedList<>();
+
+    // A(1) ~ A(N)
+    st = new StringTokenizer(br.readLine());
+    for (int i = 1; i <= N; i++) {
+      int value = Integer.parseInt(st.nextToken()); // A(i)의 Value
+
+      // 현재 추가하려는 값보다 큰 값은 제거
+      while (!deque.isEmpty() && deque.peekLast().value >= value) {
+        deque.removeLast();
+      }
+
+      deque.addLast(new Node(i, value));
+
+      // 범위를 벗어난 값 제거
+      if (deque.peekFirst().index <= i - L) {
+        deque.removeFirst();
+      }
+
+      bw.write(deque.getFirst().value +  " ");
     }
+    bw.flush();
+    bw.close();
+  }
+}
 
-    public static void main(String[] args) throws IOException {
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int L = Integer.parseInt(st.nextToken());
-        Deque<Node> deque = new LinkedList<>();
+class Node {
+  int index;
+  int value;
 
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            Node node = new Node(i, Integer.parseInt(st.nextToken()));
-
-            // 뒤에서부터 현재 node값 보다 큰 node들 삭제 (정렬)
-            while (!deque.isEmpty() && deque.getLast().value >= node.value) {
-                deque.removeLast();
-            }
-
-            // 현재 node 추가
-            deque.addLast(node);
-
-            // 범위를 넘지 않도록 앞에서부터 삭제
-            while (!deque.isEmpty() && node.idx - deque.getFirst().idx >= L ) {
-                deque.removeFirst();
-            }
-
-            // deque의 맨 앞에있는 값이 결국 최솟값 
-            bw.write(deque.getFirst().value +  " ");
-        }
-        bw.flush();
-        bw.close();
-    }
+  Node(int index, int value) {
+    this.index = index;
+    this.value = value;
+  }
 }
