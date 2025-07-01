316760623b403a09bc8003e5b3d15998af854871
junga970 <111635735+junga970@users.noreply.github.com>
2025-07-01 23:28:39 +0900

    [Bronze II] Title: 대표값2, Time: 124 ms, Memory: 14704 KB -BaekjoonHub

---
+++ 백준/Bronze/2587. 대표값2/대표값2.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int[] arr = new int[5];
+        int sum = 0;
+
+        for (int i = 0; i < 5; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+            sum += arr[i];
+        }
+
+        Arrays.sort(arr);
+        int average = sum / 5;
+        int median = arr[2];
+
+        bw.write(average + "\n");
+        bw.write(median + "\n");
+
+        bw.flush();
+        bw.close();
+    }
+}
