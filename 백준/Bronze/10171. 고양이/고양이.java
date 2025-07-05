77275af3d73ab60744d67a60ed78b140ce4240e7
junga970 <111635735+junga970@users.noreply.github.com>
2025-07-05 23:02:57 +0900

    [Bronze V] Title: 고양이, Time: 96 ms, Memory: 13976 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+public class Main {
+    
+    public static void main(String[] args) throws IOException {
+        
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        bw.write("\\    /\\\n");
+        bw.write(" )  ( ')\n");
+        bw.write("(  /  )\n");
+        bw.write(" \\(__)|\n");
+        
+        bw.flush();
+        bw.close();
+    }
+}
