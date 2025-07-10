79f653bad9d9443d0f9d6ccb840a56d8ed0dcec5
junga970 <111635735+junga970@users.noreply.github.com>
2025-07-10 22:22:28 +0900

    [Bronze II] Title: 나머지, Time: 116 ms, Memory: 14052 KB -BaekjoonHub

---
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.HashSet;
+
+public class Main {
+    
+    public static void main(String[] args) throws IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int i = 0; i < 10; i++) {
+            int num = Integer.parseInt(br.readLine());
+            set.add(num % 42);
+        }
+
+        bw.write(String.valueOf(set.size()));
+        bw.flush();
+        bw.close();
+    }
+}
