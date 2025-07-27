8d35370c5c703060687abf46d48f32b54095ec58
junga970 <111635735+junga970@users.noreply.github.com>
2025-07-27 23:48:17 +0900

    [Bronze V] Title: 문자열, Time: 120 ms, Memory: 16000 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.java
@@ -0,0 +1,18 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int t = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < t; i++) {
+            String s = br.readLine();
+            bw.write(s.charAt(0) + "" + s.charAt(s.length() - 1) + "\n");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
